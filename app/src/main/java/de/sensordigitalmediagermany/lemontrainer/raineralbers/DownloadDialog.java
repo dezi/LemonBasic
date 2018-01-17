fec0619726258f1d13d28a8e3a5947d6da60b176
dezi <dezi@kappa-mm.de>
2018-01-17 14:31:38 +0100

    PC preparation

--- app/src/main/java/de/sensordigitalmediagermany/lemontrainer/raineralbers/DownloadDialog.java
+++ app/src/main/java/de/sensordigitalmediagermany/lemontrainer/raineralbers/DownloadDialog.java
@@ -1,15 +1,19 @@
 package de.sensordigitalmediagermany.lemontrainer.raineralbers;
 
 import android.content.Context;
+import android.view.Gravity;
 import android.widget.LinearLayout;
 import android.graphics.Color;
 import android.view.View;
+import android.widget.TextView;
 
 public class DownloadDialog extends DialogView
 {
     private static final String LOGTAG = DownloadDialog.class.getSimpleName();
 
     private ProgressBar downloadProgress;
+    private TextView percentView;
+    private TextView sizeView;
 
     public DownloadDialog(Context context)
     {
@@ -31,6 +35,32 @@ public class DownloadDialog extends DialogView
         Simple.setSizeDip(dialogItems, Simple.WC, Simple.WC);
         Simple.setMarginBottomDip(dialogItems, Defines.PADDING_LARGE);
 
+        LinearLayout progressItems = new LinearLayout(getContext());
+        progressItems.setOrientation(LinearLayout.HORIZONTAL);
+        Simple.setSizeDip(progressItems, Simple.MP, Simple.WC);
+        Simple.setMarginBottomDip(progressItems, Defines.PADDING_NORMAL);
+
+        dialogItems.addView(progressItems);
+
+        percentView = new TextView(getContext());
+        percentView.setTextColor(Defines.COLOR_DIALOG_INFOS);
+        percentView.setTypeface(infosFont);
+        percentView.setAllCaps(Defines.isInfosAllCaps);
+        Simple.setSizeDip(percentView, Simple.WC, Simple.WC);
+        Simple.setTextSizeDip(percentView, Defines.FS_DIALOG_INFO);
+
+        progressItems.addView(percentView);
+
+        sizeView = new TextView(getContext());
+        sizeView.setTextColor(Defines.COLOR_DIALOG_INFOS);
+        sizeView.setTypeface(infosFont);
+        sizeView.setGravity(Gravity.END);
+        sizeView.setAllCaps(Defines.isInfosAllCaps);
+        Simple.setSizeDip(sizeView, Simple.MP, Simple.WC);
+        Simple.setTextSizeDip(sizeView, Defines.FS_DIALOG_INFO);
+
+        progressItems.addView(sizeView);
+
         downloadProgress = new ProgressBar(getContext());
         downloadProgress.setWidthDip(Defines.PROGRESS_DIALOG_WIDTH);
         downloadProgress.setColors(Color.BLACK, Color.WHITE);
@@ -42,6 +72,21 @@ public class DownloadDialog extends DialogView
 
     public void setProgressLong(long current, long total)
     {
+        if (total > 0)
+        {
+            long percent = current * 100 / total;
+            String percentstr = percent + " %";
+
+            percentView.setText(percentstr);
+
+            String ckb = Simple.formatDecimal(current / 1024);
+            String tmb = Simple.formatDecimal(total / (1024 * 1000));
+
+            String sizestr = Simple.getTrans(getContext(), R.string.download_size, ckb, tmb);
+
+            sizeView.setText(sizestr);
+        }
+
         downloadProgress.setProgressLong(current, total);
     }
 }
