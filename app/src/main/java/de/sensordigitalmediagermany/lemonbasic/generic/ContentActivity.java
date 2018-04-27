96fdd08d756aa15387f826645125bcf52ee0ddf2
Dennis Zierahn <dezi@kappa-mm.de>
2018-04-27 08:50:41 +0200

    Check in.

--- app/src/main/java/de/sensordigitalmediagermany/lemonbasic/generic/ContentActivity.java
+++ app/src/main/java/de/sensordigitalmediagermany/lemonbasic/generic/ContentActivity.java
@@ -1,5 +1,6 @@
 package de.sensordigitalmediagermany.lemonbasic.generic;
 
+import android.content.IntentFilter;
 import android.graphics.Typeface;
 import android.widget.TextView;
 import android.view.Gravity;
@@ -9,7 +10,14 @@ import android.util.Log;
 
 import org.json.JSONObject;
 
-public class ContentActivity extends ContentBaseActivity
+import de.sensordigitalmediagermany.lemonbasic.purchase.IabBroadcastReceiver;
+import de.sensordigitalmediagermany.lemonbasic.purchase.IabHelper;
+import de.sensordigitalmediagermany.lemonbasic.purchase.IabResult;
+import de.sensordigitalmediagermany.lemonbasic.purchase.Inventory;
+import de.sensordigitalmediagermany.lemonbasic.purchase.Purchase;
+import de.sensordigitalmediagermany.lemonbasic.purchase.Security;
+
+public class ContentActivity extends ContentBaseActivity implements IabBroadcastReceiver.IabBroadcastListener
 {
     private static final String LOGTAG = ContentActivity.class.getSimpleName();
 
@@ -88,13 +96,97 @@ public class ContentActivity extends ContentBaseActivity
         updateContent();
 
         ApplicationBase.handler.postDelayed(doAutomaticRefresh, Defines.AUTO_REFRESH_SECONDS * 1000);
+
+        initPurchases();
     }
 
-    private void purchases()
-    {
+    IabHelper mHelper;
+    IabBroadcastReceiver mBroadcastReceiver;
 
+    private void initPurchases()
+    {
         String base64EncodedPublicKey = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA0vVFnaH14P/p9q3P12k5gOClOpt/JlS066YNbT/2D7d8kVluFtuGhDofnbJiSEBFHhTmQNoIhN2tDB+qJ2aG7EhcnULxiNXp2GYaYOQ4ygGgmZbqaoTRr0Hz2wfMihci7sgM81Mzlhuq78TcEo/5+eMa4Eaxec5S90tYruXh05XIPKOlQEoBOKDb+WQTCIrxYn9IK3h/v3fsmfrmZhjTYX+bVvZAheIumbN73ITh7PgTFn8slqAJvaczDzrlCrI+iLEl59L2zGgSqBpN+bJRInDju1+zPMSZALLfCR8z9bRglCVTgHSWYG3uDtLbcvqIb6LEJ/AROpFn3uFrPmGZUQIDAQAB";
 
+        mHelper = new IabHelper(this, base64EncodedPublicKey);
+        mHelper.enableDebugLogging(true);
+
+        Log.d(LOGTAG, "Starting setup.");
+
+        mHelper.startSetup(new IabHelper.OnIabSetupFinishedListener()
+        {
+            public void onIabSetupFinished(IabResult result)
+            {
+                Log.d(LOGTAG, "Setup finished.");
+
+                if (!result.isSuccess())
+                {
+                    Log.e(LOGTAG, "Problem setting up in-app billing: " + result);
+                    return;
+                }
+
+                // Have we been disposed of in the meantime? If so, quit.
+                if (mHelper == null) return;
+
+                // Important: Dynamically register for broadcast messages about updated purchases.
+                // We register the receiver here instead of as a <receiver> in the Manifest
+                // because we always call getPurchases() at startup, so therefore we can ignore
+                // any broadcasts sent while the app isn't running.
+                // Note: registering this listener in an Activity is a bad idea, but is done here
+                // because this is a SAMPLE. Regardless, the receiver must be registered after
+                // IabHelper is setup, but before first call to getPurchases().
+                mBroadcastReceiver = new IabBroadcastReceiver(ContentActivity.this);
+                IntentFilter broadcastFilter = new IntentFilter(IabBroadcastReceiver.ACTION);
+                registerReceiver(mBroadcastReceiver, broadcastFilter);
+
+                // IAB is fully set up. Now, let's get an inventory of stuff we own.
+                Log.d(LOGTAG, "Setup successful. Querying inventory.");
+                try
+                {
+                    mHelper.queryInventoryAsync(mGotInventoryListener);
+                }
+                catch (IabHelper.IabAsyncInProgressException e)
+                {
+                    Log.e(LOGTAG, "Error querying inventory. Another async operation in progress.");
+                }
+            }
+        });
+    }
+
+    IabHelper.QueryInventoryFinishedListener mGotInventoryListener = new IabHelper.QueryInventoryFinishedListener()
+    {
+        public void onQueryInventoryFinished(IabResult result, Inventory inventory)
+        {
+            Log.d(LOGTAG, "Query inventory finished.");
+
+            // Have we been disposed of in the meantime? If so, quit.
+            if (mHelper == null) return;
+
+            // Is it a failure?
+            if (result.isFailure())
+            {
+                Log.e(LOGTAG, "Failed to query inventory: " + result);
+                return;
+            }
+
+            Log.d(LOGTAG, "Query inventory was successful.");
+
+            Log.d(LOGTAG, "Initial inventory query finished; enabling main UI.");
+        }
+    };
+
+    @Override
+    public void receivedBroadcast()
+    {
+        Log.d(LOGTAG, "Received broadcast notification. Querying inventory.");
+
+        try
+        {
+            mHelper.queryInventoryAsync(mGotInventoryListener);
+        }
+        catch (IabHelper.IabAsyncInProgressException e)
+        {
+            Log.e(LOGTAG, "Error querying inventory. Another async operation in progress.");
+        }
     }
 
     private final Runnable doAutomaticRefresh = new Runnable()
